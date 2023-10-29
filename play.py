30057c488aa9815f911316a8b725eb571d65d9c7
q <73285347+Desperadus@users.noreply.github.com>
2023-10-29 22:27:23 +0100

    improve parsing

--- play.py
+++ play.py
@@ -1,6 +1,18 @@
-from video import play_ascii_video_framerate
+from main import play_ascii_video_framerate
 import sys
 
-# read arguments
+# Check if the user provided at least one argument
+if len(sys.argv) < 2:
+    print("Usage: python script_name.py <input_file> [frame_rate]")
+    sys.exit(1)
 
-play_ascii_video_framerate(sys.argv[1], int(sys.argv[2]))
+# Get the input file path from the first argument
+input_file = sys.argv[1]
+
+# Set a default frame rate (e.g., 27) if it's not provided
+frame_rate = 27
+if len(sys.argv) >= 3:
+    frame_rate = int(sys.argv[2])
+
+# Call the function with the provided input file and frame rate
+play_ascii_video_framerate(input_file, frame_rate)
