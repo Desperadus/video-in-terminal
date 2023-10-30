94a313f752e37fae6222d364d490a7688bd41ba6
q <73285347+Desperadus@users.noreply.github.com>
2023-10-30 12:33:40 +0100

    add option to play video in loop

--- main.py
+++ main.py
@@ -68,8 +68,7 @@ def video_to_ascii_256(video_path, output_path, width, height):
     while success:
         cv2.imwrite("frame.jpg", image)
         frames.append(
-            "[s[?25l" +
-            image_to_ascii_256_optimized2("frame.jpg", width, height)
+            "[s[?25l" + image_to_ascii_256_optimized2("frame.jpg", width, height)
             if first
             else image_to_ascii_256_optimized2("frame.jpg", width, height)
         )
@@ -79,11 +78,19 @@ def video_to_ascii_256(video_path, output_path, width, height):
         os.remove("frame.jpg")
 
 
-def play_ascii_video_framerate(file_path, frame_rate=1):
+def play_ascii_video_framerate(file_path, frame_rate=27, loop=False):
     save_cursor()
     hide_cursor()
     clear_screen()
     try:
+        if loop:
+            while True:
+                with open(file_path, "r") as file:
+                    frames = file.read().split("\n\n")
+                    for frame in frames:
+                        # os.system("clear")
+                        sys.stdout.write(frame)
+                        time.sleep(1 / frame_rate)
         with open(file_path, "r") as file:
             frames = file.read().split("\n\n")
             for frame in frames:
@@ -95,40 +102,6 @@ def play_ascii_video_framerate(file_path, frame_rate=1):
         show_cursor()
     restore_cursor()
     show_cursor()
-
-
-def image_to_ascii_256_optimized(image_path, width, height):
-    image = Image.open(image_path)
-    image = image.resize((width, height))
-
-    ascii_str = ""
-    last_ansi_code = None
-    run_length = 0
-
-    for y in range(height):
-        for x in range(width):
-            pixel = image.getpixel((x, y))
-            r, g, b = pixel
-            ansi_code = 16 + (r // 51) * 36 + (g // 51) * 6 + (b // 51)
-            brightness = int((r + g + b) / 3)
-            ascii_char = " "  # A single space as the ASCII character
-
-            if last_ansi_code == ansi_code:
-                run_length += 1
-            else:
-                if run_length > 0:
-                    ascii_str += f"{' ' * run_length}"
-                ascii_str += f"\033[48;5;{ansi_code}m"
-                run_length = 1
-                last_ansi_code = ansi_code
-
-        if run_length > 0:
-            ascii_str += f"{' ' * run_length}\033[0m"
-        run_length = 0
-        last_ansi_code = None
-        ascii_str += "\n"
-
-    return ascii_str
 
 
 def image_to_ascii_256_optimized2(image_path, width, height):
@@ -195,6 +168,11 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
+        "--loop",
+        action="store_true",
+        help="Play video in loop",
+    )
+    parser.add_argument(
         "-u",
         "--url",
         type=str,
@@ -221,4 +199,4 @@ if __name__ == "__main__":
     height = int(dimensions[1])
 
     video_to_ascii_256(input_file, output_file, width, height)
-    play_ascii_video_framerate(output_file, frame_rate)
+    play_ascii_video_framerate(output_file, frame_rate, args.loop)
